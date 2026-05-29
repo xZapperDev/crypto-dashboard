950de953dfcf82ab8688b585a735d421af69bc5f
xZapperDev <xzapperdev@gmail.com>
2026-05-29 19:45:23 -0400

    Cambio de rutas en v1 y v2.

--- v2/src/App.jsx
+++ v2/src/App.jsx
@@ -3,7 +3,7 @@ import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/crypto-dashboard/v2/">
+    <BrowserRouter basename="./">
       <Routes>
         <Route path="/" element={<Dashboard />} />
       </Routes>
